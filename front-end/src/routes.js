d708cc188d0577960fcb291a3ddaebd2e1e8930c
JC Melati <hello@jcmelati.com>
2020-10-29 18:20:16 +0100

    Implement basic ProposalCreate structure

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import CreateAccount from './pages/CreateAccount/CreateAccount';
 import Security from './pages/Security/Security';
-import PropsalCreate from './pages/ProposalCreate/ProposalCreate';
+import ProposalCreate from './pages/ProposalCreate/ProposalCreate';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 const Routes = () => (
@@ -23,13 +24,13 @@ const Routes = () => (
                 <Security />
             </Route>
             <Route path="/proposal-create">
-                <PropsalCreate />
+                <ProposalCreate />
             </Route>
             <Route path="/dashboard">
                 <Dashboard />
             </Route>
         </Switch>
-    </Router >
+    </Router>
 );
 
 export default Routes;
