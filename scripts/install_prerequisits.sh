b881ae6dd7d2f46d8070b4559bb464f5a3e2dfb0
Jack Tanner <jack@jackandtheblockstalk.com>
2022-03-18 17:50:30 +1000

    Added  comments  to  reboot  for  prerequisits

--- scripts/install_prerequisits.sh
+++ scripts/install_prerequisits.sh
@@ -6,9 +6,9 @@ sudo apt update
 
 # Docker and docker-compose
 sudo apt install docker docker-compose -y
-# Reboot
+sudo shutdown now -r # needed for next line to take effect
 sudo usermod -aG docker ${USER}
-# Roboot
+sudo shutdown now -r # needed for previous line to take effect
 
 # nvm with node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
