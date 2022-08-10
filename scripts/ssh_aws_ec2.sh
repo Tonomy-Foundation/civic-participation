d6a424e61f4d6d97d63bfe6b42f818e9a13924be
Jack Tanner <jack@jackandtheblockstalk.com>
2022-08-10 12:02:01 +0200

    Add instructions for easy steup on AWS

--- scripts/ssh_aws_ec2.sh
+++ scripts/ssh_aws_ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod 400 ./keys/ec2.pem
-ssh -i ./keys/ec2.pem ubuntu@ec2-3-8-196-95.eu-west-2.compute.amazonaws.com
+ssh -v -i ./keys/ec2.pem ubuntu@ec2-35-180-242-177.eu-west-3.compute.amazonaws.com
 
 
 # cd civic-participation/
@@ -12,3 +12,17 @@ ssh -i ./keys/ec2.pem ubuntu@ec2-3-8-196-95.eu-west-2.compute.amazonaws.com
 # ./app.sh init prod            (if you need to reset blockchain or recompile contracts)
 # check services run correctly with:
 # ./app.sh logs [service]
+
+# To set up for Ubuntu server (no SSL on Ubuntu)
+# make sure ports 22, 3000, 4000, 8080, 8081 and 8888 are open
+# you may want to run ./build.sh on each of the contracts manually, then run `./app.sh init fast`
+# in front-end/src/settings.json
+# if (process.env.REACT_APP_NODE_ENV === "production") {
+#     settings.env = "production"
+#     settings.eosio.nodeos = "http://ec2-35-180-242-177.eu-west-3.compute.amazonaws.com:4000";
+#     settings.dfuseOptions.network = "";
+#     settings.eosio.blockExplorerUrl = "http://ec2-35-180-242-177.eu-west-3.compute.amazonaws.com:8080";
+#     settings.dfuseOptions.network = "ec2-35-180-242-177.eu-west-3.compute.amazonaws.com:8080"
+#     settings.dfuseOptions.secure = true;
+# }
+# then `./app.sh start prod`
